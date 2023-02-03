3e1532c3454c705d9669a1d791554eaefea61a2e
GrandEmetak <JekHamond@tutanota.com>
2023-02-03 16:54:45 +0500

    fix: update schema info

--- bd/insert.sql
+++ bd/insert.sql
@@ -1,3 +1,1 @@
-insert into licenses(licence_id, description, organization_id, product_name, licence_type, comment)
-VALUES ('0235431845', 'SoftwareProduct', 'optimaGrowth', 'Ostock', 'full', 'update until march 2023 include'),
-       ('0235431844', 'SoftwareProduct', 'VizusGrowth', 'Ostock', 'full', 'update until march 2024 include');
+
