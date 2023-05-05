44a23d2c9f914d2d73850264881cdbd384d3c311
GrandEmetak <JekHamond@tutanota.com>
2023-05-05 20:59:50 +0500

    fix: updated comment info

--- src/main/java/com/optimagrowth/certificate/CertificateServiceApplication.java
+++ src/main/java/com/optimagrowth/certificate/CertificateServiceApplication.java
@@ -3,14 +3,10 @@ package com.optimagrowth.certificate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @Slf4j
 @SpringBootApplication
-@RefreshScope
-@EnableFeignClients
-@EnableBinding(Sink.class)
 //@EnableCaching
 public class CertificateServiceApplication {
 
