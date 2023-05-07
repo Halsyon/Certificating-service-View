a478950c11cc17454ad565b4f3513c6fae3975a2
GrandEmetak <JekHamond@tutanota.com>
2023-05-07 14:14:53 +0500

    fix: updated model info

--- src/main/java/com/optimagrowth/certificate/events/model/OrganizationChangeModel.java
+++ src/main/java/com/optimagrowth/certificate/events/model/OrganizationChangeModel.java
@@ -1,12 +1,6 @@
 package com.optimagrowth.certificate.events.model;
 
-import lombok.*;
 
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrganizationChangeModel {
 //...
 }
